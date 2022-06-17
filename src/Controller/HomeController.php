9b590fdbac01292fcf40754448f91c5f6e8e7042
Antonio Gagliarducci <antonio@projects.rocks>
2022-06-17 18:27:26 +0200

    Moved the routers inside the controllers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    #[Route('/', name: 'app_homepage')]
     public function getPage(): Response
     {
         return $this->render('homepage.html');
--- src/Controller/HouseController.php
+++ src/Controller/HouseController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HouseController extends AbstractController
 {
     public function getPage(): Response
+    #[Route('/house/{id}', name: 'app_house')]
     {
         return $this->render('house.html');
+    #[Route('/house/create', name: 'app_house')]
     }
 }
