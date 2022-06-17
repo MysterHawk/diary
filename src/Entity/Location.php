de4c1e962692d82e12b003f7cc0b777ade93d1e0
Antonio Gagliarducci <antonio@projects.rocks>
2022-06-17 18:36:17 +0200

    Created controller location contact and listing

---
+++ src/Entity/Location.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\LocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+#[ApiResource]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $address;
+
+    #[ORM\Column(type: 'integer')]
+    private $address_number;
+
+    #[ORM\Column(type: 'integer')]
+    private $zip_code;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getAddressNumber(): ?int
+    {
+        return $this->address_number;
+    }
+
+    public function setAddressNumber(int $address_number): self
+    {
+        $this->address_number = $address_number;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zip_code;
+    }
+
+    public function setZipCode(int $zip_code): self
+    {
+        $this->zip_code = $zip_code;
+
+        return $this;
+    }
+}
