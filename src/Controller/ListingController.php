94f6d84f056de1328504ddb0b53f00ae03e9f463
Antonio Gagliarducci <antonio@projects.rocks>
2022-06-21 12:04:42 +0200

    Move antwerpHouses listing to twig template

--- src/Controller/ListingController.php
+++ src/Controller/ListingController.php
@@ -11,6 +11,6 @@ class ListingController extends AbstractController
     #[Route('/listing/antwerp/houses', name: 'app_listing_houses_antwerp')]
     public function listing_houses_antwerp(): Response
     {
-        return $this->render('listing/antwerpHouses.html');
+        return $this->render('listing/antwerpHouses.html.twig');
     }
 }
--- src/Controller/getListing.php
+++ src/Controller/getListing.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\House;
 use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\AsController;
-use Symfony\Component\HttpFoundation\Response;
 
 #[AsController]
 class getListing extends AbstractController
